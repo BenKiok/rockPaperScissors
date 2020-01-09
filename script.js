32d19bb1767f271b172c9d97fe5e79a325d01e1d
Ben Kioko <benedictmkioko@gmail.com>
2020-01-09 02:39:47 -0500

    Move script to external file, initialize script upon browser load To separate structure and function, move JS from script tag in HTML file to separate script.js file. Add game function call at the beginning of the script so that the game begins immediately upon loading.

---
+++ script.js
@@ -0,0 +1,80 @@
+game();
+
+function playerMove () {
+    let move = prompt("What is your move?").toLowerCase();
+    if (move !== 'rock' && move !== 'paper' && move !== 'scissors') {
+        alert("Invalid play. Try again.");
+        return playerMove();
+    }
+    
+    return move;
+}
+
+function computerPlay () {
+    let plays = ['rock', 'paper', 'scissors'];
+    let rand = Math.floor(Math.random() * 3);
+
+    return plays[rand];
+}
+
+function playRound (pSelect, cpuSelect) {
+    if (pSelect === cpuSelect) {
+        alert("It's a tie! Play again!");
+        return;
+    } else if (pSelect === "rock") {
+        if (cpuSelect === "paper") {
+            alert(`You lose! ${cpuSelect} beats ${pSelect}!`);
+            return false;
+        } else {
+            alert(`You win! ${pSelect} beats ${cpuSelect}!`);
+            return true;
+        }
+    } else if (pSelect === "paper") {
+        if (cpuSelect === "scissors") {
+            alert(`You lose! ${cpuSelect} beats ${pSelect}!`);
+            return false;
+        } else {
+            alert(`You win! ${pSelect} beats ${cpuSelect}!`);
+            return true;
+        }
+    } else {
+        if (cpuSelect === "rock") {
+            alert(`You lose! ${cpuSelect} beats ${pSelect}!`);
+            return false;
+        } else {
+            alert(`You win! ${pSelect} beats ${cpuSelect}!`);
+            return true;
+        }
+    }
+}
+
+function game() {
+    let didPlayerWin;
+    let pWins = 0;
+    let cpuWins = 0;
+    let tryAgain;
+
+    for (let i = 0; i < 5; i++) {
+        didPlayerWin = playRound(playerMove(), computerPlay());
+
+        if (didPlayerWin) {
+            pWins++;
+        } else if (!didPlayerWin) {
+            cpuWins++;
+        }
+    }
+
+    if (pWins > cpuWins) {
+        alert(`You win! ${pWins} out of 5 rounds`);
+    } else if (pWins < cpuWins) {
+        alert(`You lose! ${pWins} out of 5 rounds`);
+    } else {
+        alert(`You tied! ${pWins} out of 5 rounds`);
+    }
+
+    tryAgain = confirm("Play again?");
+
+    if (tryAgain) {
+        game();
+    }
+}
